d8e1e16eb7d56e1ec1e096c0cc34ec604136ce44
Leonid Lebedinsky <lennylebedinsky@gmail.com>
2024-09-20 15:58:31 -0700

    Client breaking connection scenario

--- internal/chat/client.go
+++ internal/chat/client.go
@@ -1,6 +1,8 @@
 package chat
 
 import (
+	"log"
+
 	"github.com/gorilla/websocket"
 	"github.com/lennylebedinsky/chatter/internal/domain"
 )
@@ -14,14 +16,24 @@ type UserSocket struct {
 	user *domain.User
 	conn *websocket.Conn
 
+	broadcaster *Broadcaster
+
 	send chan []byte
+
+	logger *log.Logger
 }
 
-func NewUserSocket(user *domain.User, conn *websocket.Conn) *UserSocket {
+func NewUserSocket(
+	user *domain.User,
+	conn *websocket.Conn,
+	broadcaster *Broadcaster,
+	logger *log.Logger) *UserSocket {
 	return &UserSocket{
-		user: user,
-		conn: conn,
-		send: make(chan []byte),
+		user:        user,
+		conn:        conn,
+		broadcaster: broadcaster,
+		send:        make(chan []byte),
+		logger:      logger,
 	}
 }
 
@@ -35,4 +47,24 @@ func (s *UserSocket) ID() string {
 
 func (s *UserSocket) Send() chan []byte {
 	return s.send
+}
+
+func (s *UserSocket) ReadLoop() {
+	defer func() {
+		s.broadcaster.Unregister() <- s
+		s.conn.Close()
+	}()
+	for {
+		_, _, err := s.conn.ReadMessage()
+		if err != nil {
+			if closeErr, ok := err.(*websocket.CloseError); ok {
+				s.logger.Printf("Connection closed for client %s: %v\n", s.ID(), closeErr)
+			} else {
+				s.logger.Printf("Error reading message for client %s: %v\n", s.ID(), err)
+			}
+			break
+		}
+		//message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		//c.hub.broadcast <- message
+	}
 }
